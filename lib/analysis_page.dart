f11cb0c3ddec8997e8297c2ece40ba0f7c5db525
Kenta Wakasa <kenta@mba.local>
2020-12-10 14:02:39 +0900

    test commit

---
+++ lib/analysis_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnalyisPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: const Text('hoge')),
+    );
+  }
+}
---
+++ lib/start_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'analysis_page.dart';
+
+class StartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
+    const _defPadding = 48.0;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(_defPadding),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  width: _size.width,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: const FittedBox(
+                    child: Text('VOICE',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: _size.width,
+                child: RaisedButton(
+                  child: const Text(
+                    '音声波形を見る',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacement<void, void>(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AnalyisPage(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
