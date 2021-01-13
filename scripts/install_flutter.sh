8e33591e61d861d31c27b15661819d9d5dc6b8cc
kenta-wakasa <wakasa.kenta.pg@gmail.com>
2021-01-13 11:21:11 +0900

    hoge

---
+++ scripts/install_flutter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# アップデートなどをかけるために path 情報を生成する
+root=$(pwd) 
+flutter_dir=$root/flutter
+flutter_bin_dir=$flutter_dir/bin
+flutter=$flutter_bin_dir/flutter
+
+# flutter を clone してくる
+git clone https://github.com/flutter/flutter.git
+
+# upgrade をかける
+$flutter upgrade
+
+# version 情報を取得
+flutter_full_version=$(cat $flutter_dir/version)
+vers=(${flutter_full_version//./ })
+flutter_version=${vers[0]}.${vers[1]}
+
+# 出力する
+echo "$flutter_full_version"
+echo "FLUTTER_FULL_VERSION=$flutter_full_version" >> $GITHUB_ENV #envに追加
